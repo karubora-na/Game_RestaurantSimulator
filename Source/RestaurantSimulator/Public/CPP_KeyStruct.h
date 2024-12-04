f55d64e62d38585026eb0449d0e0805d6890929c
KARUBO\ykink <ykinktbti7@gmail.com>
2024-12-04 16:53:12 +0900

    キーに使用する構造体を作成。メンバはint32型の3つの変数

---
+++ Source/RestaurantSimulator/Public/CPP_KeyStruct.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CPP_KeyStruct.generated.h"
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct FCPP_KeyStruct {
+
+	GENERATED_BODY()
+
+	int32 _first_key;
+	int32 _second_key;
+	int32 _third_key;
+
+};
