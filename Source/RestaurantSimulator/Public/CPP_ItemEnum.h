1df0b2cc7366ba66d8afb2adee572afa9b1523e0
KARUBO\ykink <ykinktbti7@gmail.com>
2024-12-04 17:17:39 +0900

    アイテム番号を保存する列挙型のヘッダを作成。

---
+++ Source/RestaurantSimulator/Public/CPP_ItemEnum.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CPP_ItemEnum.generated.h"
+/**
+ * 
+ */
+UENUM(BlueprintType)
+enum class ECPP_ItemEnum : uint8 {
+
+	CAN
+};
